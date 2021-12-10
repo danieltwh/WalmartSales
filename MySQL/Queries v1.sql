cbe2cde24db900b306ff772421add4f8b93e4a72
danieltwh <71161486+danieltwh@users.noreply.github.com>
2021-12-10 21:34:37 +0800

    Init

---
+++ MySQL/Queries v1.sql
@@ -0,0 +1,15 @@
+USE Walmart;
+
+SELECT * FROM Region;
+
+SELECT * FROM DMZ;
+
+SELECT * FROM Demand
+LIMIT 2000;
+
+SELECT 
+    table_schema 'Database Name',
+    SUM(data_length + index_length) 'Size in Bytes',
+    ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) 'Size in MiB'
+FROM information_schema.tables 
+GROUP BY table_schema;
